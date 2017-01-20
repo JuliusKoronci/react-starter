d43592de3c3d659e1cf27d00a0a6b5c980689c29
LanSystems <mbabinska@bitbucket.org>
2017-01-20 15:59:04 +0100

    template for left task section - part 2

--- src/app/views/templates/main/task/left/commentForm.jsx.js
+++ src/app/views/templates/main/task/left/commentForm.jsx.js
@@ -8,16 +8,15 @@ const commentForm = () => {
                 <li className="named_tab"><a href="#">Email</a></li>
             </ul>
             <ul id="tabs_1_content" className="uk-switcher uk-margin">
-                <li>
+                <li className="uk-active">
                     <textarea cols="30" rows="4" id="border-full" className="md-input"
                               placeholder="Add reply..."/>
-                    <p/>
+
                     <input type="checkbox" name="checkbox_demo_inline_mercury" id="checkbox_demo_inline_1"
                            data-md-icheck/>
                     <label htmlFor="checkbox_demo_inline_1" className="inline-label margin15">Internal Note </label>
                     <a className="md-btn-flat-primary" href="#">Add attachment</a>
                     <a className="md-btn md-btn-primary md-btn-small md-btn-wave-light" href="#">Add</a>
-                    <hr/>
                 </li>
                 <li>
                     <div className="mail-formular">
@@ -25,9 +24,16 @@ const commentForm = () => {
                         <input id="table-item" type="text" className="md-input" placeholder="Cc:"/>
                         <input id="table-item" type="text" className="md-input" placeholder="Subject:"/>
                         <textarea cols="30" rows="4" className="md-input" placeholder=""/>
+
+                        <input type="checkbox" name="checkbox_demo_inline_mercury" id="checkbox_demo_inline_1"
+                               data-md-icheck/>
+                        <label htmlFor="checkbox_demo_inline_1" className="inline-label margin15">Internal Note </label>
+                        <a className="md-btn-flat-primary" href="#">Add attachment</a>
+                        <a className="md-btn md-btn-primary md-btn-small md-btn-wave-light" href="#">Add</a>
                     </div>
                 </li>
             </ul>
+            <hr/>
         </div>
     );
 };
--- src/app/views/templates/main/task/left/commentList.jsx.js
+++ src/app/views/templates/main/task/left/commentList.jsx.js
@@ -1,8 +1,225 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 
 const commentList = () => {
     return (
-        <div></div>
+        <div className="timeline">
+            <div className="timeline_item">
+                <div className="timeline_icon timeline_icon_success">
+                    <p className="md-user-letters md-bg-cyan">ML</p>
+                </div>
+
+                <article className="uk-comment">
+                    <header className="uk-comment-header" style={{paddingTop: '8px'}}>
+                        <div className="uk-grid" data-uk-grid-margin>
+                            <div className="uk-width-medium-3-4">
+                                <p className="uk-comment-title">
+                                    Marcelina Lueilwitz
+                                    <span className="uk-comment-title uk-text-italic">posted note</span>
+                                </p>
+                            </div>
+                            <div className="uk-width-medium-1-4">
+                                <div className="uk-comment-meta text-allign-right">
+                                    24/Jun/15 14:26
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+
+                    <div style={{marginLeft: '10px'}}>
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                            sed
+                            diam nonumy eirmod tempor invidunt ut labore et dolore
+                            magna
+                            aliquyam erat, sed diam voluptua.Lorem ipsum dolor sit
+                            amet,
+                            onsetetur sadipscing elitr, sed diam nonumy eirmod
+                            tempor
+                            invidunt ut labore et dolore magna aliquyam erat, sed
+                            diam
+                            voluptua.
+                        </p>
+                    </div>
+                </article>
+            </div>
+            <hr/>
+
+            <div className="timeline_item">
+                <div className="timeline_icon timeline_icon_success">
+                    <p className="md-user-letters md-bg-cyan">ML</p>
+                </div>
+
+                <article className="uk-comment private-note" style={{paddingBottom: '1px'}}>
+                    <header className="uk-comment-header" style={{paddingTop: '8px'}}>
+                        <div className="uk-grid" data-uk-grid-margin>
+                            <div className="uk-width-medium-3-4">
+                                <p className="uk-comment-title">
+                                    Marcelina Lueilwitz
+                                    <span className="uk-comment-title uk-text-italic">posted a private note</span>
+                                </p>
+                            </div>
+                            <div className="uk-width-medium-1-4">
+                                <div className="uk-comment-meta text-allign-right">
+                                    24/Jun/15 14:26
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+
+                    <div style={{marginLeft: '10px'}}>
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                            sed
+                            diam nonumy eirmod tempor invidunt ut labore et dolore
+                            magna
+                            aliquyam erat, sed diam voluptua.Lorem ipsum dolor sit
+                            amet,
+                            consetetur sadipscing elitr, sed diam nonumy eirmod
+                            tempor
+                            invidunt ut labore et dolore magna aliquyam erat, sed
+                            diam
+                            voluptua.
+                        </p>
+                    </div>
+                </article>
+            </div>
+            <hr/>
+
+            <div className="timeline_item">
+                <div className="timeline_icon timeline_icon_primary">
+                    <i className="material-icons">&#xE0E1;</i>
+                </div>
+
+                <article className="uk-comment">
+                    <header className="uk-comment-header" style={{paddingTop: '8px'}}>
+                        <div className="uk-grid" data-uk-grid-margin>
+                            <div className="uk-width-medium-3-4">
+                                <p className="uk-comment-title">
+                                    <span className="uk-comment-title uk-text-italic">From:</span>
+                                    branislav.susta@gmail.com
+                                </p>
+                                <p className="uk-comment-title">
+                                    <span className="uk-comment-title uk-text-italic">To:  </span>
+                                    hotline@lansystems.sk
+                                </p>
+                                <p className="uk-comment-title">
+                                    <span className="uk-comment-title uk-text-italic">Cc:  </span>
+                                    hotline@lansystems.sk
+                                </p>
+                                <br/>
+                                <p className="uk-comment-title">
+                                    <span className="uk-comment-title uk-text-italic">Subject: </span>
+                                    Mail Subject
+                                </p>
+                                <br/>
+                            </div>
+
+                            <div className="uk-width-medium-1-4">
+                                <div className="uk-comment-meta text-allign-right">
+                                    24/Jun/15 14:26
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+
+                    <div style={{marginLeft: '10px'}}>
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing
+                            elitr,
+                            sed diam nonumy
+                            eirmod tempor invidunt ut labore et dolore magna
+                            aliquyam erat, sed diam
+                            voluptua.Lorem ipsum dolor sit amet, consetetur
+                            sadipscing elitr, sed
+                            diam nonumy eirmod tempor invidunt ut labore et
+                            dolore
+                            magna aliquyam
+                            erat, sed diam voluptua.
+                        </p>
+
+                        <p className="md-color-blue-900">
+                            <span>
+                                <i className="material-icons md-color-blue-900">&#xE226;</i>Priloha 1
+                            </span>
+
+                            <span>
+                                <i className="material-icons md-color-blue-900">&#xE226;</i> Priloha 2
+                            </span>
+                        </p>
+
+                    </div>
+                </article>
+            </div>
+            <hr/>
+
+            <div className="timeline_item">
+                <div className="timeline_icon timeline_icon_primary">
+                    <i className="material-icons">&#xE163;</i>
+                </div>
+
+                <article className="uk-comment">
+                    <header className="uk-comment-header" style={{paddingTop: '8px'}}>
+                        <div className="uk-grid" data-uk-grid-margin>
+                            <div className="uk-width-medium-3-4">
+                                <p className="uk-comment-title">
+                                    <span className="uk-comment-title uk-text-italic">From:</span>
+                                    branislav.susta@gmail.com
+                                </p>
+                                <p className="uk-comment-title">
+                                    <span className="uk-comment-title uk-text-italic">To:  </span>
+                                    hotline@lansystems.sk
+                                </p>
+                                <p className="uk-comment-title">
+                                    <span className="uk-comment-title uk-text-italic">Cc:  </span>
+                                    hotline@lansystems.sk
+                                </p>
+                                <br/>
+                                <p className="uk-comment-title">
+                                    <span className="uk-comment-title uk-text-italic">Subject: </span>
+                                    Mail Subject
+                                </p>
+                                <br/>
+                            </div>
+
+                            <div className="uk-width-medium-1-4">
+                                <div className="uk-comment-meta text-allign-right">
+                                    24/Jun/15 14:26
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+
+                    <div style={{marginLeft: '10px'}}>
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing
+                            elitr,
+                            sed diam nonumy
+                            eirmod tempor invidunt ut labore et dolore magna
+                            aliquyam erat, sed diam
+                            voluptua.Lorem ipsum dolor sit amet, consetetur
+                            sadipscing elitr, sed
+                            diam nonumy eirmod tempor invidunt ut labore et
+                            dolore
+                            magna aliquyam
+                            erat, sed diam voluptua.
+                        </p>
+
+                        <p className="md-color-blue-900">
+                            <span>
+                                <i className="material-icons md-color-blue-900">&#xE226;</i>Priloha 1
+                            </span>
+
+                            <span>
+                                <i className="material-icons md-color-blue-900">&#xE226;</i> Priloha 2
+                            </span>
+                        </p>
+
+                    </div>
+                </article>
+            </div>
+            <hr/>
+
+        </div>
     );
 };
 
