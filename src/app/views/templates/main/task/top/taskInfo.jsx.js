9d2e663aac9d026487c4565ed0962b06a430559c
Julius Koronci <koronci@trimbroker.com>
2017-01-20 11:22:25 +0100

    push

--- src/app/views/templates/main/task/top/taskInfo.jsx.js
+++ src/app/views/templates/main/task/top/taskInfo.jsx.js
@@ -2,9 +2,13 @@ import React, {PropTypes} from 'react';
 
 const taskInfo = ({task}) => {
     return (
-        <div>
-            {task.title}
-            <p>Created: {task.created_by.username}</p>
+        <div className="uk-width-medium-3-4">
+            <input type="text" className="header_b md-input" value="Task name"/>
+
+            <p className="uk-text-muted">
+                Created: Branislav Šusta (branislav.susta@gmail.com) | 06.12.2016
+                11:01
+            </p>
         </div>
     );
 };
