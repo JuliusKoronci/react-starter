04998fa108b2311f6cfebe7a56e7154b6989169a
Julius Koronci <koronci@trimbroker.com>
2017-01-25 12:25:14 +0100

    Datepicker

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -3,6 +3,7 @@ import '../../../node_modules/uikit/dist/css/uikit.almost-flat.min.css';
 import '../views/assets/css/login_page.css';
 import '../../../node_modules/toastr/build/toastr.min.css';
 import '../../../node_modules/nprogress/nprogress.css';
+import '../../../libs/react-datetime/css/react-datetime.css';
 
 class App extends Component {
     render() {
--- src/app/views/templates/main/task/right/closeDate.jsx.js
+++ src/app/views/templates/main/task/right/closeDate.jsx.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import DateTime from '../../../../../../../libs/react-datetime/DateTime';
 
 const closeDate = () => {
     return (
         <div className="uk-input-group" style={{marginTop: '20px'}}>
             <span className="uk-input-group-addon"><i className="material-icons">&#xE5CD;</i></span>
             <label className="uk-text-muted" htmlFor="uk_dp_1">Close date</label>
-            <input className="md-input" type="text" id="uk_dp_1" data-uk-datepicker="{format:'DD.MM.YYYY'}"/>
+            <DateTime className="md-input"/>
         </div>
     );
 };
