50b1d3105bc4b719a289697a5993c655093577a7
misog <miso.galan@gmail.com>
2017-02-23 10:49:26 +0100

    ReduxForm

--- src/app/components/Main/Settings/Smtps/Smtp.js
+++ src/app/components/Main/Settings/Smtps/Smtp.js
@@ -1,12 +1,29 @@
 import React, { Component} from 'react';
+import configResolver from '../../../../../config/configResolver';
+import NProgress from '../../../../../../node_modules/nprogress/nprogress';
 import View from '../../../../views/templates/main/settings/smtps/add_smtp.jsx';
 
 class Smtp extends Component {
 
+    constructor(props, context) {
+        super(props, context);
+        this.smtpConfig = configResolver.getSmtpConfig(props.params.smtpId);
+    }
+
+    componentWillMount() {
+        if (this.props.params.smtpId && !this.props.smtp) {
+            this.props.actions.loadEntityById(this.props.params.smtpId, this.smtpConfig);
+        }
+    }
+
+    handleSubmit = (values) => {
+    	NProgress.start();
+    	alert('ok')
+    }
 
     render() {
         return (
-            <View prop={{}} />
+            <View onSumbit={this.handleSubmit} {...this.props} />
         );
     }
 }
--- src/app/views/templates/main/settings/smtps/add_smtp.jsx.js
+++ src/app/views/templates/main/settings/smtps/add_smtp.jsx.js
@@ -1,75 +1,84 @@
 import React, {PropTypes} from 'react';
+import {Component} from 'react';
+import {connect} from 'react-redux';
+import {Field, reduxForm} from 'redux-form';
 
-const add_status = (prop) => {
-    return (
-        <div className="md-card">
-            <div className="md-card-content">
-                <div className="uk-margin-bottom" data-uk-margin>
-                    <h1 className="heading_b uk-margin-bottom">Add SMTP</h1>
-                </div>
-                <hr/>
-                <div className="uk-width-medium-1-2">
-                    <div className="uk-margin-bottom">
-                        <input type="checkbox" name="checkbox_demo_inline_mercury" id="checkbox_demo_inline_1"
-                               data-md-icheck/>
-                        <label className="uk_dp1 uk-text-muted">Active</label>
-                    </div>
-                    <div className="uk-margin-bottom">
-                        <label>email</label>
-                        <input type="text" className="md-input label-fixed"/>
-                    </div>
-                    <div className="uk-margin-bottom">
-                        <label>Server</label>
-                        <input type="text" className="md-input label-fixed"/>
-                    </div>
-                    <div className="uk-margin-bottom">
-                        <label>Port</label>
-                        <input type="text" className="md-input label-fixed"/>
-                    </div>
-                    <div className="uk-margin-bottom">
-                        <label>Login</label>
-                        <input type="text" className="md-input label-fixed"/>
-                    </div>
-                    <div className="uk-margin-bottom">
-                        <label>Password</label>
-                        <input type="text" className="md-input label-fixed"/>
-                    </div>
-                    <div className="uk-margin-bottom">
-                        <label>Project folder</label>
-                        <input type="text" className="md-input label-fixed" value="from ALL"/>
-                    </div>
-                    <div className="uk-margin-bottom">
-                        <input type="checkbox" name="checkbox_demo_inline_mercury" id="checkbox_demo_inline_1"
-                               data-md-icheck/>
-                        <label className="uk_dp1 uk-text-muted">TSL</label>
-                    </div>
-                    <div className="uk-margin-bottom">
-                        <input type="checkbox" name="checkbox_demo_inline_mercury" id="checkbox_demo_inline_1"
-                               data-md-icheck/>
-                        <label className="uk_dp1 uk-text-muted">SSL</label>
-                    </div>
+class AddSmtp extends Component {
 
-                    <h3>Test smtp</h3>
-                    <div className="uk-margin-bottom">
-                        <label>email for send test mail</label>
-                        <input type="text" className="md-input label-fixed"/>
-                    </div>
-                    <div className="uk-margin-bottom">
-                        <a className="md-btn md-btn-success" href="settings_units.html">SEND</a>
+    render() {
+        const {handleSubmit} = this.props;
+        return (
+            <div className="md-card">
+                <form onSubmit={handleSubmit}>
+                    <div className="md-card-content">
+                        <div className="uk-margin-bottom" data-uk-margin>
+                            <h1 className="heading_b uk-margin-bottom">Add SMTP</h1>
+                        </div>
+                        <hr/>
+                        <div className="uk-width-medium-1-2">
+                            <div className="uk-margin-bottom">
+                                <input type="checkbox" name="checkbox_demo_inline_mercury" id="checkbox_demo_inline_1"
+                                       data-md-icheck/>
+                                <label className="uk_dp1 uk-text-muted">Active</label>
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <label>email</label>
+                                <input type="text" className="md-input label-fixed"/>
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <label>Server</label>
+                                <input type="text" className="md-input label-fixed"/>
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <label>Port</label>
+                                <input type="text" className="md-input label-fixed"/>
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <label>Login</label>
+                                <input type="text" className="md-input label-fixed"/>
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <label>Password</label>
+                                <input type="text" className="md-input label-fixed"/>
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <label>Project folder</label>
+                                <input type="text" className="md-input label-fixed" value="from ALL"/>
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <input type="checkbox" name="checkbox_demo_inline_mercury" id="checkbox_demo_inline_1"
+                                       data-md-icheck/>
+                                <label className="uk_dp1 uk-text-muted">TSL</label>
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <input type="checkbox" name="checkbox_demo_inline_mercury" id="checkbox_demo_inline_1"
+                                       data-md-icheck/>
+                                <label className="uk_dp1 uk-text-muted">SSL</label>
+                            </div>
 
+                            <h3>Test smtp</h3>
+                            <div className="uk-margin-bottom">
+                                <label>email for send test mail</label>
+                                <input type="text" className="md-input label-fixed"/>
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <a className="md-btn md-btn-success" href="settings_units.html">SEND</a>
+
+                            </div>
+                            <div className="uk-margin-bottom">
+                                <a className="md-btn md-btn-danger" href="#">Delete</a>
+                                <button type="submit" className="md-btn md-btn-primary alignright" href="settings_smtps.html">Save</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="uk-margin-bottom">
-                        <a className="md-btn md-btn-danger" href="#">Delete</a>
-                        <a className="md-btn md-btn-primary alignright" href="settings_smtps.html">Save</a>
-                    </div>
-                </div>
+                </form>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
-add_status.propTypes = {
-    prop: PropTypes.object.isRequired
-};
+AddSmtp = reduxForm({
+    form: 'AddSmtp'
+})(AddSmtp);
 
-export default add_status;
+export default connect()(AddSmtp);
