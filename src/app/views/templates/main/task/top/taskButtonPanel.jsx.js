98746791bb348a3d8422820b1b4a3edfff22914d
bsusta <susta@lansystems.sk>
2017-09-20 11:15:57 +0200

    uprava tlačidiel editacie tasku

--- src/app/views/templates/main/task/top/taskButtonPanel.jsx.js
+++ src/app/views/templates/main/task/top/taskButtonPanel.jsx.js
@@ -1,60 +1,89 @@
-import React from 'react';
-import {Link, browserHistory} from 'react-router';
+import React from "react";
+import { Link, browserHistory } from "react-router";
 
+const taskButtonPanel = ({
+  saveAction,
+  form,
+  formInputChangeHandler,
+  deleteButton,
+  handleTaskDelete,
+  handleCancelClick,
+  errors
+}) => {
+  return (
+    <div>
+      <div className="md-btn-group">
+        <button
+          onClick={saveAction.bind(null)}
+          className="md-btn md-btn-success md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light"
+        >
+          <i className="uk-icon-save" />
+          SAVE
+        </button>
 
-const taskButtonPanel = ({saveAction, form, formInputChangeHandler, deleteButton, handleTaskDelete, handleCancelClick,errors}) => {
+        {/*<a className="md-btn md-btn-warning md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light"*/}
+        {/*href="#">*/}
+        {/*<i className="uk-icon-ban"/>*/}
+        {/*CANCEL*/}
+        {/*</a>*/}
+        <Link
+          className="md-btn md-btn-warning md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light"
+          to={"/"}
+          onClick={browserHistory.goBack}
+        >
+          <i className="uk-icon-ban" /> Cancel
+        </Link>
 
-    return (
-        <div>
-            <div className="md-btn-group">
+        {deleteButton && (
+          <a
+            className="md-btn md-btn-danger md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light"
+            href="#"
+            onClick={handleTaskDelete}
+          >
+            <i className="uk-icon-ban" />
+            DELETE
+          </a>
+        )}
 
+        {formInputChangeHandler && (
+          <a
+            className="md-btn md-btn-primary md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light"
+            href="#"
+          >
+            <i className="uk-icon-print" />
+            Print
+          </a>
+        )}
 
-                { deleteButton &&
-                <a className="md-btn md-btn-danger md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light"
-                   href="#" onClick={handleTaskDelete} >
-                    <i className="uk-icon-ban"/>
-                    DELETE
-                </a>
-                }
+        {formInputChangeHandler && (
+          <button
+            onClick={formInputChangeHandler.bind(
+              null,
+              "important",
+              !form.important
+            )}
+            className={
+              "md-btn md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light " +
+              (form.important ? " md-btn-important" : "md-btn-default")
+            }
+            href="#"
+          >
+            <i className="uk-icon-star-half-empty" />
+            Important
+          </button>
+        )}
 
-                {/*<a className="md-btn md-btn-warning md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light"*/}
-                   {/*href="#">*/}
-                    {/*<i className="uk-icon-ban"/>*/}
-                    {/*CANCEL*/}
-                {/*</a>*/}
-                <Link className="md-btn md-btn-warning md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light" to={'/'} onClick={browserHistory.goBack}><i className="uk-icon-ban"/> Cancel</Link>
-
-
-                {formInputChangeHandler && <a className="md-btn md-btn-primary md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light"
-                   href="#">
-                    <i className="uk-icon-print"/>
-                    Print
-                </a>}
-
-                {formInputChangeHandler && <button onClick={formInputChangeHandler.bind(null,'important',!form.important)} className={"md-btn md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light "+ (form.important?'md-btn-primary':'md-btn-default') }
-                   href="#">
-                    <i className="uk-icon-star-half-empty"/>
-                    Important
-                </button> }
-
-            </div>
-
-            <div className="alignright" style={{float: 'right'}}>
-                <button onClick={saveAction.bind(null)} className="md-btn md-btn-primary md-btn-small md-btn-wave-light md-btn-icon waves-effect waves-button waves-light">
-                    <i className="uk-icon-save"/>
-                    SAVE
-                </button>
-
-                {
-                    errors && errors.length>0 &&  <div>{errors.map(er=>{return <p key={er}>{er}</p>})}</div>
-
-                }
-
-            </div>
-        </div>
-    );
+        {errors &&
+        errors.length > 0 && (
+          <div>
+            {errors.map(er => {
+              return <p key={er}>{er}</p>;
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
-
-
 
 export default taskButtonPanel;
