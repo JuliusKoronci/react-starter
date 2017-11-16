c995f3746e264c356fbf797406c56941c8050f9b
bsusta <susta@lansystems.sk>
2017-11-16 21:01:46 +0100

    BS-EditTask-#f4f4f4

--- src/app/forms/general/RichText.form.js
+++ src/app/forms/general/RichText.form.js
@@ -1,90 +1,110 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import ReactQuill from 'react-quill';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import ReactQuill from "react-quill";
 
 class RichText extends Component {
+  constructor(props, context) {
+    super(props, context);
 
-    constructor(props, context) {
-        super(props, context);
+    this.state = {
+      text:
+        props.form && props.form[props.fieldName]
+          ? props.form[props.fieldName]
+          : ""
+    };
+  }
 
-        this.state = {text: props.form && props.form[props.fieldName]?props.form[props.fieldName]:''}
+  // componentDidUpdate(prevProps, prevState) {
+  //
+  //     if(prevProps!==this.props){
+  //
+  //         if(this.props.form && this.props.form[this.props.fieldName] && this.props.form[this.props.fieldName] !== prevProps.form[this.props.fieldName]){
+  //
+  //             this.setState({text: this.props.form[this.props.fieldName]});
+  //             console.log('change '+this.props.form[this.props.fieldName],prevProps.form[this.props.fieldName]);
+  //
+  //         }
+  //
+  //         // if(this.props.form && this.props.form[this.props.fieldName]){
+  //         //     this.setState({text:this.props.form[this.props.fieldName]});
+  //         // }
+  //     }
+  // }
+
+  handleChange = value => {
+    if (this.props.action) {
+      this.props.action(this.props.fieldName, value);
+    } else {
+      this.setState({ text: value });
     }
+  };
 
+  render() {
+    let val =
+      this.props.form && this.props.form[this.props.fieldName]
+        ? this.props.form[this.props.fieldName]
+        : false;
+    const value =
+      val ||
+      this.props.value ||
+      (this.props.task && this.props.task[this.props.fieldName]
+        ? this.props.task[this.props.fieldName]
+        : "");
 
-
-
-
-    // componentDidUpdate(prevProps, prevState) {
-    //
-    //     if(prevProps!==this.props){
-    //
-    //         if(this.props.form && this.props.form[this.props.fieldName] && this.props.form[this.props.fieldName] !== prevProps.form[this.props.fieldName]){
-    //
-    //             this.setState({text: this.props.form[this.props.fieldName]});
-    //             console.log('change '+this.props.form[this.props.fieldName],prevProps.form[this.props.fieldName]);
-    //
-    //         }
-    //
-    //         // if(this.props.form && this.props.form[this.props.fieldName]){
-    //         //     this.setState({text:this.props.form[this.props.fieldName]});
-    //         // }
-    //     }
-    // }
-
-
-
-    handleChange=(value)=>{
-        if(this.props.action){
-            this.props.action(this.props.fieldName,value);
-        }else {
-            this.setState({text: value});
-        }
+    let modules = {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { indent: "-1" },
+          { indent: "+1" }
+        ],
+        ["link", "image"],
+        ["clean"]
+      ]
     };
 
-    render() {
+    let formats = [
+      "header",
+      "bold",
+      "italic",
+      "underline",
+      "strike",
+      "blockquote",
+      "list",
+      "bullet",
+      "indent",
+      "link",
+      "image"
+    ];
 
-        let val=this.props.form && this.props.form[this.props.fieldName]?this.props.form[this.props.fieldName]:false;
-        const value = val || this.props.value || (this.props.task&&this.props.task[this.props.fieldName]?this.props.task[this.props.fieldName]:'');
+    return (
+      <div>
+        {this.props.label && (
+          <label className="uk-text-muted">{this.props.label}</label>
+        )}
 
-        let modules = {
-            toolbar: [
-                [{'header': [1, 2, false]}],
-                ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-                [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-                ['link', 'image'],
-                ['clean']
-            ],
-        };
-
-        let formats = [
-            'header',
-            'bold', 'italic', 'underline', 'strike', 'blockquote',
-            'list', 'bullet', 'indent',
-            'link', 'image'
-        ];
-
-
-        return (
-
-            <div>
-            {this.props.label && <label className="uk-text-muted">{this.props.label}</label>}
-
-            <ReactQuill valuee={this.state.text} theme="snow"
-                        value={value}
-                        modules={modules} formats={formats}
-                        onChange={this.handleChange}/>
-            </div>
-        );
-    }
+        <ReactQuill
+          valuee={this.state.text}
+          theme="snow"
+          value={value}
+          modules={modules}
+          formats={formats}
+          onChange={this.handleChange}
+          style={{ background: "white" }}
+        />
+      </div>
+    );
+  }
 }
 
-
 function mapStateToProps(state) {
-    const task = state.tasks.task.data;
-    return {
-        task: task
-    };
+  const task = state.tasks.task.data;
+  return {
+    task: task
+  };
 }
 
 export default connect(mapStateToProps)(RichText);
--- src/app/views/templates/layout.jsx.js
+++ src/app/views/templates/layout.jsx.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import Header from './header.jsx'
+import React from "react";
+import Header from "./header.jsx";
 // import Sidebar from './sidebar.jsx';
-import Sidebar from '../../components/Main/Sidebar/Sidebar';
+import Sidebar from "../../components/Main/Sidebar/Sidebar";
 
-export default ({children, user, actions, filters, sidebarIsMinified, projects, archivedProjects, tags, params, loggedUserAcl, location}) => {
-    return (
-        <div>
-            <Header user={user} logout={actions.logout} sidebarIsMinified={sidebarIsMinified} toggleSidebar={actions.toggleSidebar} />
-            <Sidebar filters={filters} projects={projects} archivedProjects={archivedProjects} tags={tags} params={params} loggedUserAcl={loggedUserAcl} location={location} sidebarIsMinified={sidebarIsMinified} />
-            <div id="page_content">
-                <div id="page_content_inner">
-                    <div className="md-card">
-                            {/*{children}*/}
-                        {React.cloneElement(children, {projects, tags})}
-                    </div>
-                </div>
-            </div>
+export default ({
+  children,
+  user,
+  actions,
+  filters,
+  sidebarIsMinified,
+  projects,
+  archivedProjects,
+  tags,
+  params,
+  loggedUserAcl,
+  location
+}) => {
+  return (
+    <div>
+      <Header
+        user={user}
+        logout={actions.logout}
+        sidebarIsMinified={sidebarIsMinified}
+        toggleSidebar={actions.toggleSidebar}
+      />
+      <Sidebar
+        filters={filters}
+        projects={projects}
+        archivedProjects={archivedProjects}
+        tags={tags}
+        params={params}
+        loggedUserAcl={loggedUserAcl}
+        location={location}
+        sidebarIsMinified={sidebarIsMinified}
+      />
+      <div id="page_content">
+        <div id="page_content_inner">
+          <div className="md-card">
+            {/*{children}*/}
+            {React.cloneElement(children, { projects, tags })}
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
